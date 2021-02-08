259f4b4d2363cc4d0f824ffa8bceaabae84e012a
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-02-08 17:21:07 -0500

    adds to test.

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -9,34 +9,28 @@ import CheckoutForm from "./CheckoutForm";
 test("form header renders", () => {
     render(<CheckoutForm/>);
     const header = screen.queryByText(/Checkout Form/i);
+    expect(header).toBeInTheDocument();
 });
 
-
-// test("form header renders", () => {
-//     render(<CheckoutForm/>);
-//     const header = screen.queryByText(/Checkout Form/i);
-//     expect(header).toBeInTheDocument();
-// });
-
-// test("form shows success message on submit with form details", async () => {
-//     render(<CheckoutForm/>);
-//     const firstName = screen.getByLabelText(/First Name:/i);
-//     const lastName = screen.getByLabelText(/Last Name:/i);
-//     const address = screen.getByLabelText(/Address:/i);
-//     const city = screen.getByLabelText(/City:/i);
-//     const state = screen.getByLabelText(/State:/i);
-//     const zip = screen.getByLabelText(/Zip:/i);
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm/>);
+    const firstName = screen.getByLabelText(/First Name:/i);
+    const lastName = screen.getByLabelText(/Last Name:/i);
+    const address = screen.getByLabelText(/Address:/i);
+    const city = screen.getByLabelText(/City:/i);
+    const state = screen.getByLabelText(/State:/i);
+    const zip = screen.getByLabelText(/Zip:/i);
     
-//     userEvent.type(firstName, "Warren");
-//     userEvent.type(lastName, "Longmire");
-//     userEvent.type(address, "1111 3rd street");
-//     userEvent.type(city, "Philadelphia");
-//     userEvent.type(state, "PA");
-//     userEvent.type(zip, "19143");
+    userEvent.type(firstName, "Warren");
+    userEvent.type(lastName, "Longmire");
+    userEvent.type(address, "1111 3rd street");
+    userEvent.type(city, "Philadelphia");
+    userEvent.type(state, "PA");
+    userEvent.type(zip, "19143");
 
-//     const button = screen.getByRole("button");
-//     userEvent.click(button);
+    const button = screen.getByRole("button");
+    userEvent.click(button);
 
-//     const messageContainer = await screen.findByTestId('successMessage');
-//     expect(messageContainer).toBeInTheDocument();
-// });
+    const messageContainer = await screen.findByTestId('successMessage');
+    expect(messageContainer).toBeInTheDocument();
+});
