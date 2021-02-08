5a24f5d131f93f4ab5bee8335091f721aa638ec0
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-02-08 16:58:47 -0500

    codegrade update.

--- codegrade/fixtures/setup.bash
+++ codegrade/fixtures/setup.bash
@@ -4,6 +4,6 @@ shopt -s extglob
 
 rm -rf "$STUDENT/src/codegrade" || true
 mkdir "$STUDENT/src/codegrade"
-mv "$FIXTURES/setupTests.js" "$STUDENT/src";
-mv "$FIXTURES/*.test.js" "$STUDENT/src/codegrade/tests"
+mv "$FIXTURES/setupTests.js" "$STUDENT/src"
+mv "$FIXTURES/*.test.js" "$STUDENT/src/codegrade"
 mv "$FIXTURES/jest.config.js" "$STUDENT"
